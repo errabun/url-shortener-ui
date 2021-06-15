333305203287d6cd6e2041abffe0747f799c0b4d
Eric Rabun <errabun@gmail.com>
2021-06-15 09:38:49 -0600

    Create postUrl api call function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,15 @@ export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
       .then(response => response.json())
 }
+
+export const postUrl = (longUrl, title) => {
+  return fetch('http://localhost:3001/api/v1/urls', {
+    method: 'POST',
+    body: JSON.stringify({
+      long_url: longUrl,
+      title: title
+    }),
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(response => response.json())
+}
